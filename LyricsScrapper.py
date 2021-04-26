f9c92adeb5743dbb3924b4a56bd0719040a60c2c
othman.mhaouty <othman.mhaouty@telecom-sudparis.eu>
2021-04-26 21:03:30 +0200

    Changing details on the script to enhance the efficiency

--- LyricsScrapper.py
+++ LyricsScrapper.py
@@ -10,10 +10,10 @@ import lyricsgenius as genius
 
 file = open("./1PLIKÉ140.txt", "w")  #Change the name of the file you want to write the lyrics on it, depends on the artist(s) you want to work on.
 #You can find your creds by creating an account on : https://genius.com/api-clients
-creds="YOUR CREDENTIELS HERE"
+creds="RqeV2PSIPDxz4hFa64f4AUKA2F1L9ATtlsGntL_b1aFBEVkX4AWK26Tf0dH0wcKg"
 #We are excluding everything that could be a remix, a featuring or a live version, because we want to train our script on the artist.
 genius = genius.Genius(creds,
-                             skip_non_songs=True, excluded_terms=["(Remix)", "(Live)", "(Featuring)", "(ft.)", "featuring", "ft."],
+                             skip_non_songs=True, excluded_terms=["(Remix)", "(Live)", "(Featuring)", "(ft.)", "featuring", "ft.", "avec", "with"],
                              remove_section_headers=True)
 
 artists = ['1PLIKé140'] # You can choose a lot of artists, here we are focusing on one artist to use GPT on his texts
@@ -23,13 +23,13 @@ def lyrics_scrapper(arr, k):  # Write lyrics of k songs by each artist in arr
     c = 0  
     for name in arr:
         try:
-            songs = (genius.search_artist(name, max_songs=k)).songs #We can also change the script to use it without max_songs
+            songs = (genius.search_artist(name)).songs #We can also change the script to use it without max_songs
             s = [song.lyrics for song in songs]
-            file.write("\n \n   <End of the song>   \n \n".join(s))  # Allows us and GPT to restrict the number of songs.
+            file.write("\n \n   <End of the song>   \n \n".join(s))  # Allows us and GPT to restrict the length of the songs.
             c += 1
-            print("Songs found:{len(s)}")
+            print("Songs found:"+ len(s))
         except:  #
-            print("No song found for {name}: {c}")
+            print("No song found")
 
 
 lyrics_scrapper(artists, 50)
