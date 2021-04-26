9374803ba6ca3d139bb446c78b4649cf2fddb053
othman.mhaouty <othman.mhaouty@telecom-sudparis.eu>
2021-04-26 20:49:39 +0200

    Adding few comments

--- LyricsScrapper.py
+++ LyricsScrapper.py
@@ -11,6 +11,7 @@ import lyricsgenius as genius
 file = open("./1PLIKÉ140.txt", "w")  #Change the name of the file you want to write the lyrics on it, depends on the artist(s) you want to work on.
 #You can find your creds by creating an account on : https://genius.com/api-clients
 creds="YOUR CREDENTIELS HERE"
+#We are excluding everything that could be a remix, a featuring or a live version, because we want to train our script on the artist.
 genius = genius.Genius(creds,
                              skip_non_songs=True, excluded_terms=["(Remix)", "(Live)", "(Featuring)", "(ft.)", "featuring", "ft."],
                              remove_section_headers=True)
